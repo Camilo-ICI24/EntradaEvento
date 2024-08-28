ded73069b9fd80eda875a719f161ee9b2acb5056
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-08-28 14:34:41 -0400

    Actualización y corrección masiva de errores

--- src/main/java/org/example/EntradaEvento.java
+++ src/main/java/org/example/EntradaEvento.java
@@ -2,6 +2,8 @@ package org.example;
 
 import java.util.Objects;
 
+import static java.lang.Integer.parseInt;
+
 class EntradaEvento {
 
     public static void main (String[] args) {
@@ -11,49 +13,51 @@ class EntradaEvento {
     }
 
     public static void crearMatrizInformacion() {
-        String[][] informacion = new String[10][5];
+        String[][] informacion = new String[10][5]; /*En esta matriz estará almacenada toda la
+        información de los asistentes*/
     }
 
-    public static boolean verificarEdad(String[][] m) {
-            for (int fila = 0; fila < m.length; fila++) {
-                if (Integer.parseInt(m[fila][1]) >= 18) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        return false;
-    }
-
-    public static boolean verificarSiEsta(String[][] m, String persona) {
-        for (int fila = 0; fila < m.length; fila++) {
-            if (Objects.equals(m[fila][0], persona)) {
-                return true;
-            } else {
-                return false;
-            }
+    public static boolean verificarEdad(String[][] m, int fila) { /* Este método debe devolver true si la
+    persona en la fila especificada tiene 18 años o más, y false en caso contrario*/
+        if (Integer.parseInt(m[fila][1]) >= 18) {
+            return true;
         }
         return false;
     }
 
-   public static String verificarBoleto(String[][] m, String persona) {
-        boolean existe = verificarSiEsta(m, persona);
-        if (existe) {
-            return m[Integer.parseInt(persona)][2];
+   public static String verificarBoleto(String[][] m, int fila) {
+        if (m[fila][2] == null) {
+            return "False";
         }
+        return m[fila][2];
    }
 
-    public static boolean validarInvitados(String[][] m, String persona) {
-        boolean existe = verificarSiEsta(m, persona);
-        if (existe) {
-            if ()
+    public static boolean validarInvitados(String[][] m, int fila) {
+        if (Integer.parseInt(m[fila][3]) > 2) { //Solo debe aplicar para invitados con entrada VIP
+            return false;
         }
+        return true;
     }
 
-    public static int aforoDisponible() {
+    public static int aforoDisponible(String[][] m) {
+        int aforoVipUsado = 0;
+        int aforoGeneralUsado = 0;
+        for (int fila = 0; fila < m.length; fila++) {
+            if ((m[fila][2]).toLowerCase() == "general") {
+                aforoGeneralUsado += 1;
+            } else-if ((m[fila][2]).toLowerCase() == "vip") {
+                aforoVipUsado += 1;
+            }
+        }
+        System.out.println("Los aforos disponibles para general y para VIP son, respectivamente: ");
+        return 30 - aforoGeneralUsado;
+        return 50 - aforoVipUsado;
     }
 
-    public static boolean ingresarPersona() {
+    public static void ingresarPersona(String[][] m, int fila) {
+        if ((m[fila][4]).toLowerCase() == "true") {
+
+        }
 
     }
 
